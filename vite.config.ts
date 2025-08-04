a1b6d28d23e6bc59a41e11bdef76651b856336ce
alienforge <alienforgenft@gmail.com>
2025-08-04 15:57:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/alienforge-brand-showcase/', // <-- KLJUČNO za GitHub Pages podfolder
+  base: '/alienforge-brand-showcase/',    // <-- obavezno
+  build: {
+    outDir: 'docs',                      // <-- build ide u docs/
+  },
   plugins: [react()],
-});
+})
