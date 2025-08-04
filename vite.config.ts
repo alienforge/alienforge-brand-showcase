2c99ac516f31a96b5aa9b5d1458aeea899e52ef0
Marko Rosic <marko24rosic@gmail.com>
2025-08-04 16:31:22 +0200

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: '/alienforge-brand-showcase/',    // <-- obavezno
-  build: {
-    outDir: 'docs',                      // <-- build ide u docs/
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: '/alienforge-brand-showcase/',
+  server: {
+    host: "::",
+    port: 8080,
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
